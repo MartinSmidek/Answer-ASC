22650cae42361c744891b29e5b8e74597ca1430e
ledni <lednickyo@gmail.com>
2026-01-26 17:00:03 +0100

    Prihlaska_ASC - test

--- prihlaska.log.php
+++ prihlaska.log.php
@@ -18,3 +18,32 @@
 2025.4/1    1 2025-10-22 18:20:49   12 <b style='color:green'>POBYT </b> ... Ondřej Lednický id_pobyt=6 mail=ondra.lednicky@volny.cz ip=127.0.0.1
 2025.4/1    1 2025-10-24 14:05:22    7 DOTAZ= ... už je přihlášen Martin Šmídek  mail=martin@smidek.eu ip=127.0.0.1
 2025.4/1    1 2025-10-24 14:09:19   13 KLIENT ... Martin Šmídek id_osoba=6, id_rodina=2 mail=martin@smidek.eu ip=127.0.0.1
+2025.4/1    1 2025-12-18 14:16:51   62 <b style='color:blue'>REGIST</b> ... lednicky@yahoo.com mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    1 2025-12-18 14:17:46   62   <b style='color:blue'>dupl</b> ... pro Ondřej Lednický nalezena id_osoba=764 mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    1 2025-12-18 14:17:46   62   <b style='color:blue'>dupl</b> ... pro Marie Lednická nalezena id_osoba=765 mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    1 2025-12-18 14:17:46   62 <b style='color:green'>POBYT </b> ... Ondřej Lednický, Marie Lednická id_pobyt=13 mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    1 2025-12-19 22:44:14    ? MAIL? 4 &times; lednicky@yahoo.com ...  764-a-501 764-a-566 764-a-567 764-a-568 (id_osoba-role-id_rodina) mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    1 2025-12-19 22:44:21   62 DOTAZ= ... už je přihlášen Ondřej Lednický  mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    1 2025-12-19 22:44:46   63 <b style='color:blue'>REGIST</b> ... pako@seznam.cz mail=pako@seznam.cz ip=127.0.0.1
+2025.4/1    7 2026-01-23 10:44:00    ? <b style='color:red'>ERROR</b> CHYBA smtp/E: stream_socket_enable_crypto(): SSL operation failed with code 1. OpenSSL Error messages:
+error:1416F086:SSL routines:tls_process_server_certificate:certificate verify failed mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-23 10:44:23    ? MAIL? 4 &times; lednicky@yahoo.com ...  764-a-501 764-a-566 764-a-567 764-a-568 (id_osoba-role-id_rodina) mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-23 10:44:26   64 KLIENT ... Ondřej Lednický id_osoba=764, id_rodina=566 mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-23 10:45:08    ? MAIL? 4 &times; lednicky@yahoo.com ...  764-a-501 764-a-566 764-a-567 764-a-568 (id_osoba-role-id_rodina) mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-23 10:45:10   65 KLIENT ... Ondřej Lednický id_osoba=764, id_rodina=566 mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-26 11:30:10    ? <b style='color:red'>ERROR</b> CHYBA smtp/E: stream_socket_enable_crypto(): SSL operation failed with code 1. OpenSSL Error messages:
+error:1416F086:SSL routines:tls_process_server_certificate:certificate verify failed mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-26 11:30:10    ? <b style='color:red'>ERROR</b> CHYBA smtp/E: stream_socket_enable_crypto(): SSL operation failed with code 1. OpenSSL Error messages:
+error:1416F086:SSL routines:tls_process_server_certificate:certificate verify failed mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-26 13:24:49    ? MAIL? 4 &times; lednicky@yahoo.com ...  764-a-501 764-a-566 764-a-567 764-a-568 (id_osoba-role-id_rodina) mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-26 13:24:52   66 KLIENT ... Ondřej Lednický id_osoba=764, id_rodina=566 mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-26 13:30:14    ? MAIL? 4 &times; lednicky@yahoo.com ...  764-a-501 764-a-566 764-a-567 764-a-568 (id_osoba-role-id_rodina) mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-26 13:30:16   67 KLIENT ... Ondřej Lednický id_osoba=764, id_rodina=566 mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-26 16:34:12    ? MAIL? 4 &times; lednicky@yahoo.com ...  764-a-501 764-a-566 764-a-567 764-a-568 (id_osoba-role-id_rodina) mail=lednicky@yahoo.com ip=127.0.0.1
+2025.4/1    7 2026-01-26 16:34:14   68 KLIENT ... Ondřej Lednický id_osoba=764, id_rodina=566 mail=lednicky@yahoo.com ip=127.0.0.1
+asc.4/1    7 2026-01-26 16:44:08    ? MAIL? 4 &times; lednicky@yahoo.com ...  764-a-501 764-a-566 764-a-567 764-a-568 (id_osoba-role-id_rodina) mail=lednicky@yahoo.com ip=127.0.0.1
+asc.4/1    7 2026-01-26 16:44:09   69 KLIENT ... Ondřej Lednický id_osoba=764, id_rodina=566 mail=lednicky@yahoo.com ip=127.0.0.1
+asc.4/1    7 2026-01-26 16:48:31   69 <b style='color:green'>POBYT </b> ... Ondřej Lednický, Marie Ledmnická id_pobyt=14 mail=lednicky@yahoo.com ip=127.0.0.1
+asc.4/1    7 2026-01-26 16:48:55   70 <b style='color:blue'>REGIST</b> ... ondra.lednicky@volny.cz mail=ondra.lednicky@volny.cz ip=127.0.0.1
+asc.4/1    7 2026-01-26 16:52:17   71 <b style='color:blue'>REGIST</b> ... ondra.lednicky@volny.cz mail=ondra.lednicky@volny.cz ip=127.0.0.1
+asc.4/1    7 2026-01-26 16:53:24   71 <b style='color:green'>POBYT </b> ... Ondřej LednickýVolny id_pobyt=15 mail=ondra.lednicky@volny.cz ip=127.0.0.1
--- prihlaska_asc.php
+++ prihlaska_asc.php
@@ -17,4 +17,5 @@
 //}
 
 //$_GET['org']= 'asc-test/asc';
-require_once("prihlaska_2025.4.php"); 
+require_once("prihlaska_asc.4.php"); 
+
