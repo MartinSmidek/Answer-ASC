5d6e4e0df598ab35dec8f61bbaf9a8fee87a5948
ledni <lednickyo@gmail.com>
2026-04-27 00:27:04 +0200

    Přihláška ASC: oprava pokračování v rozpracované přihlášce
    Dvě související opravy ve form_MO / form_R / form_J:
    
    1) Skeleton merge — pokud uložená vars_json neobsahuje některý
       klíč $vars->form (starší verze formuláře), doplní se default
       z aktuálního skeletonu. Existující hodnoty z vars_json se
       nepřepíšou. Řeší pre-existing chybu "Undefined property:
       stdClass::$rodina" při pokračování v rozpracované přihlášce
       uložené starší verzí formuláře (řádky 1529, 1540, 1545, 1550).
    
    2) Save/restore $vars->typ_akce — uživatelova volba varianty
       formuláře (J/R/MO) se nyní explicitně ukládá do vars_json
       v log_write_changes a obnovuje v log_find_saved /
       log_load_changes. Dříve se restoroval ze $saved->form->typ,
       což je ale typ akce ($akce->p_typ), ne uživatelova volba.
       Pro R akci se solo registrací (registrace_jako_klient('J',...))
       se po pokračování nesprávně zobrazoval form_R místo form_J.
       Pro starší rozpracované bez explicitního typ_akce se použije
       původní fallback na $saved->form->typ.

--- prihlaska.log.php
+++ prihlaska.log.php
@@ -109,3 +109,88 @@ asc.4/1    7 2026-04-26 22:42:29  102 <b style='color:blue'>REGIST</b> ... ondre
 asc.4/1    7 2026-04-26 22:42:53  102   wait ... mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
 asc.4-edit     7 2026-04-26 20:48:17    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=64 email=lednicky@yahoo.com admin user=1 mail=? ip=127.0.0.1
 asc.4-edit     7 2026-04-26 20:48:22    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=65 email=lednicky@yahoo.com admin user=1 mail=? ip=127.0.0.1
+asc.4/1    7 2026-04-26 23:45:07  100 DOTAZ= ... už je přihlášen Ondřej Lednický  mail=lednicky@yahoo.com ip=127.0.0.1
+asc.4/1    7 2026-04-26 23:45:13   99 DOTAZ= ... už je přihlášen Ondřej LednickýVolny  mail=ondra.lednicky@volny.cz ip=127.0.0.1
+asc.4/1    7 2026-04-26 23:45:20  103    102 ... ondrej.lednicky@mibcon.cz mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-26 23:45:20  103 <b style='color:red'>CATCH</b> Undefined property: stdClass::$rodina na řádku  1529:    |  740: form_R (0)  |  723: formular ()  |  146: klient (-1/0,0)  mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-26 23:55:44  104    103 ... ondrej.lednicky@mibcon.cz mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-26 23:56:20  104   wait ... mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-26 23:56:37  105    104 ... ondrej.lednicky@mibcon.cz mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:15    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=105 email=ondrej.lednicky@mibcon.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:16    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=104 email=ondrej.lednicky@mibcon.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:18    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=103 email=ondrej.lednicky@mibcon.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:19    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=102 email=ondrej.lednicky@mibcon.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:20    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=98 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:21    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=97 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:23    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=96 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:24    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=95 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:25    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=94 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:26    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=93 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:27    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=92 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:28    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=91 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:29    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=90 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:30    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=89 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:31    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=88 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:32    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=87 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:34    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=86 email=lednicky@yahoo.com admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:35    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=85 email=lednicky@yahoo.com admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:37    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=84 email=lednicky@yahoo.com admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:38    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=83 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:40    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=82 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:41    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=81 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:42    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=80 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:43    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=79 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:45    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=78 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:46    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=77 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:47    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=76 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:49    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=75 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:51    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=74 email=lednicky@yahoo.com admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:53    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=73 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:56    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=66 email=lednicky@yahoo.com admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:57:59    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=67 email=lednicky@yahoo.com admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 21:58:00    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=70 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4/1    7 2026-04-26 23:58:21  106 <b style='color:blue'>REGIST</b> ... ondrej.lednicky@mibcon.cz mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-26 23:58:31  106   wait ... mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-26 23:58:47  107    106 ... ondrej.lednicky@mibcon.cz mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:05:41  108    107 ... ondrej.lednicky@mibcon.cz mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:06:42    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=108 email=ondrej.lednicky@mibcon.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:06:45    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=107 email=ondrej.lednicky@mibcon.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:06:47    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=106 email=ondrej.lednicky@mibcon.cz admin user=1 mail=? ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:07:04  109 <b style='color:blue'>REGIST</b> ... ondrej.lednicky@mibcon.cz mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:07:11  109   wait ... mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:07:25  110    109 ... ondrej.lednicky@mibcon.cz mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:07:33  110   wait ... mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:07:44  111    110 ... ondrej.lednicky@mibcon.cz mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:08:06  111   wait ... mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:08:17  112    111 ... ondrej.lednicky@mibcon.cz mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:08:23  112   <b style='color:blue'>dupl</b> ... pro Ondřej Lednický nalezena id_osoba=764 mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:08:23  112 <b style='color:green'>POBYT </b> ... Ondřej Lednický id_pobyt=19 mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:08:36  112 DOTAZ= ... už je přihlášen Ondřej Lednický  mail=ondrej.lednicky@mibcon.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:16:57  113 KLIENT ... Anežka Hesová id_osoba=600, id_rodina=0 mail=anezce@seznam.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:17:16  113   wait ... mail=anezce@seznam.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:17:23  114    113 ... Anežka Hesová id_osoba=600, id_rodina=0 mail=anezce@seznam.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:17:40  115    114 ... Anežka Hesová id_osoba=600, id_rodina=0 mail=anezce@seznam.cz ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:18:14    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=113 email=anezce@seznam.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:18:17    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=114 email=anezce@seznam.cz admin user=1 mail=? ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:18:32  116    115 ... Anežka Hesová id_osoba=600, id_rodina=0 mail=anezce@seznam.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:18:43  116 <b style='color:green'>POBYT </b> ... Anežka Hesová id_pobyt=20 mail=anezce@seznam.cz ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:19:08    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=115 email=anezce@seznam.cz admin user=1 mail=? ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:20:00  117 KLIENT ... Marek Cúth id_osoba=761, id_rodina=498 mail=marek.cuth@gmail.com ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:20:28  117   wait ... mail=marek.cuth@gmail.com ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:20:38  118    117 ... Marek Cúth id_osoba=761, id_rodina=498 mail=marek.cuth@gmail.com ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:20:56  119    118 ... Marek Cúth id_osoba=761, id_rodina=498 mail=marek.cuth@gmail.com ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:21:57  119 <b style='color:green'>POBYT </b> ... Marek Cúth, Veronika Cúthová id_pobyt=21 mail=marek.cuth@gmail.com ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:22:20  120 <b style='color:blue'>REGIST</b> ... artax@seznam.cz mail=artax@seznam.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:23:06  120   wait ... mail=artax@seznam.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:23:21  121    120 ... artax@seznam.cz mail=artax@seznam.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:23:50  122    121 ... artax@seznam.cz mail=artax@seznam.cz ip=127.0.0.1
+asc.4/1    7 2026-04-27 00:24:09  122 <b style='color:green'>POBYT </b> ... Ondřej LednickýArtax, Ráchel Lednická id_pobyt=22 mail=artax@seznam.cz ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:24:28    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=121 email=artax@seznam.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:24:30    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=120 email=artax@seznam.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:24:32    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=118 email=marek.cuth@gmail.com admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:24:33    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=117 email=marek.cuth@gmail.com admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:24:36    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=111 email=ondrej.lednicky@mibcon.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:24:37    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=110 email=ondrej.lednicky@mibcon.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:24:39    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=109 email=ondrej.lednicky@mibcon.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:24:41    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=72 email=ondra.lednicky@volny.cz admin user=1 mail=? ip=127.0.0.1
+asc.4-edit     7 2026-04-26 22:24:43    ? <b style='color:red'>DELETE</b> ... rozpracovaná id_prihlaska=68 email=lednicky@yahoo.com admin user=1 mail=? ip=127.0.0.1
